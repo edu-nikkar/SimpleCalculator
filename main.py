1c97fe51e3ad9e87a7784984f20b1837aa4984b3
Nikodemus Karlsson <nikodemus.karlsson@taby.se>
2025-11-02 15:20:27 +0000

    Lade till logik för fyra räknesätt.

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ val = input("Val: ")
 tal1 = float(input("Ange första talet: "))
 tal2 = float(input("Ange andra talet: "))
 
+if val == "1":
+    print("Resultat:", tal1 + tal2)
+elif val == "2":
+    print("Resultat:", tal1 - tal2)
+elif val == "3":
+    print("Resultat:", tal1 * tal2)
+elif val == "4":
+    print("Resultat:", tal1 / tal2)
+else:
+    print("Ogiltigt val.")
+
