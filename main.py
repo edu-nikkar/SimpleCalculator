908f53479f5f060be48c141c3781e95041a19825
Nikodemus Karlsson <nikodemus.karlsson@taby.se>
2025-11-02 15:24:36 +0000

    Lade till loop för flera beräkningar och hantering av felaktig input.

--- main.py
+++ main.py
@@ -23,3 +23,34 @@ elif val == "4":
 else:
     print("Ogiltigt val.")
 
+while True:
+    print("\nVälj operation:")
+    print("1. Addition")
+    print("2. Subtraktion")
+    print("3. Multiplikation")
+    print("4. Division")
+    print("5. Avsluta")
+
+    val = input("Val: ")
+
+    if val == "5":
+        print("Avslutar...")
+        break
+
+    try:
+        tal1 = float(input("Ange första talet: "))
+        tal2 = float(input("Ange andra talet: "))
+
+        if val == "1":
+            print("Resultat:", tal1 + tal2)
+        elif val == "2":
+            print("Resultat:", tal1 - tal2)
+        elif val == "3":
+            print("Resultat:", tal1 * tal2)
+        elif val == "4":
+            print("Resultat:", tal1 / tal2)
+        else:
+            print("Ogiltigt val.")
+    except ValueError:
+        print("Fel: Ange giltiga tal.")
+
